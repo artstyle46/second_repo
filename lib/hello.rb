2374e19c90bfe6fa037bd30022cf34f9eccd3984
Asit Kumar Singh <asit1992@gmail.com>
2015-08-02 12:58:20 +0530

    Hello uses greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,7 @@
+require 'greeter'
 # default is " world"
 # author is asit kumar singh/"asit1992@gmail.com"
-name = ARGV.first || "World"
-puts "Hello, #{name}!"
+name = ARGV.first || "world"
+
+greeter = Greeter.new(name)
+puts greeter.greet
